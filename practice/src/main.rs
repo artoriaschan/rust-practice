c5d26d4575c47591b5073ef48e918c4486b09650
artoriaschan <danielovelol@163.com>
2021-12-14 14:30:17 +0800

    fix: 修改配置文件

---
+++ practice/src/main.rs
@@ -0,0 +1,14 @@
+#![allow(dead_code)]
+#![allow(unused_parens)]
+#![allow(unused_variables)]
+
+mod _struct;
+mod collections;
+mod enumerate;
+mod error_handling;
+mod general_concept;
+mod ownership;
+
+fn main() {
+    error_handling::run();
+}
